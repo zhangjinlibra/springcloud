2ff8cff0ee8ac648fba6e9a15c7d0d81e4bda887
zhangjinlibra <zhangjinlibra@163.com>
2019-07-12 13:30:26 +0800

    cloud

---
+++ springconfig-svn-3366/src/main/java/com/inus/ms/SpringConfigSvn3366_app.java
@@ -0,0 +1,18 @@
+
+package com.inus.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+    @ SpringBootApplication
+    @ EnableConfigServer
+public class SpringConfigSvn3366_app
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringConfigSvn3366_app.class, args);
+    }
+
+}
